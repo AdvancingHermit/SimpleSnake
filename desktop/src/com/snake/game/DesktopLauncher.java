beb5525e1d229c36072a4e83af576f930ba966dd
AdvancingHermit <christiannkya@outlook.com>
2024-01-06 17:51:02 +0100

    done

--- desktop/src/com/snake/game/DesktopLauncher.java
+++ desktop/src/com/snake/game/DesktopLauncher.java
@@ -14,9 +14,9 @@ public class DesktopLauncher {
     public static void main(String[] arg) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the width of the grid: ");
-        int n = scanner.nextInt();
+        int n = Math.min(Math.max(5, scanner.nextInt()), 100);
         System.out.println("Enter the height of the grid: ");
-        int m = scanner.nextInt();
+        int m = Math.min(Math.max(5, scanner.nextInt()), 100);
         scanner.close();
 
         Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
